dfa694f4c959d3746bad1e12ccae3607545cb6c2
Vitaly Osipov <vital-nvl@mail.ru>
2023-02-03 21:39:36 +0300

    server

---
+++ server.py
@@ -0,0 +1,19 @@
+import socket
+
+sock = socket.socket()
+
+sock.bind(('', 30303))
+print("Connecting port 30303")
+
+sock.listen(5)
+conn, addr = sock.accept()
+print("Connected >> ", addr)
+while True:
+    data = conn.recv(8192)
+    msg = data.decode()
+    print('Message >>  ', msg)
+    if msg == 'exit':
+        break
+    resp = input('Answer >> ')
+
+    conn.send(resp.encode())
