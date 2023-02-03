5647b41edcbf196b3cfee5c97e23c54d82d93c3d
Vitaly Osipov <vital-nvl@mail.ru>
2023-02-03 21:39:16 +0300

    client

---
+++ client.py
@@ -0,0 +1,13 @@
+import socket
+
+sock = socket.socket()
+sock.connect(('127.0.0.1', 30303))
+print ('Connected to server!\n')
+while True:
+    msg = input('Message >> ')
+
+    sock.send(msg.encode())
+    if msg == 'exit':
+        break
+    data = sock.recv(8192)
+    print('Answer >> ', data)
